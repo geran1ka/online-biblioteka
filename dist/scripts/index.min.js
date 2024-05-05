5b612c69bcdf17a917cce617377c2bd25b4697c8
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-05-05 20:37:57 +0300

    gulp

---
+++ dist/scripts/index.min.js
@@ -0,0 +1,14 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/scripts/modules/test.js
+const sum = (a, b) => a + b;
+
+;// CONCATENATED MODULE: ./src/scripts/index.js
+
+
+
+console.log('sum(3, 5): ', sum(3, 5));
+/******/ })()
+;
